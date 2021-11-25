7af16095d1023d2b0c291a29ccd748c6fed7b5d4
TomBastin <bastintom41@gmail.com>
2021-11-25 16:40:42 +0100

    progress1.1

--- lib/test.dart
+++ lib/test.dart
@@ -1,2 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:rat/main.dart';
 
+void main() => runApp(MyFirstApp());
+
+class MyFirstApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title: 'ShoppingList',
+      theme: ThemeData(primarySwatch: Colors.red) ,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Container();
+  }
+}
